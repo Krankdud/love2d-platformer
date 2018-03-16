8371885dac42141d61c9ba612d1e78c72932e432
charlie <charlieschuy@gmail.com>
2018-03-16 15:47:14 -0700

    Initial commit

---
+++ main.lua
@@ -0,0 +1,30 @@
+local autobatch = require "lib.autobatch"
+local debugGraph = require "lib.debugGraph"
+local gamestate = require "lib.hump.gamestate"
+local tick = require "lib.tick"
+
+assets = require("lib.cargo").init("assets")
+
+menu = {}
+
+function love.load(arg)
+	tick.rate = 1 / 60
+
+	fpsGraph = debugGraph:new("fps", 0, 0)
+	memGraph = debugGraph:new("mem", 0, 30)
+
+	gamestate.registerEvents()
+	gamestate.switch(menu)
+end
+
+function love.update(dt)
+	require("lib.lurker").update()
+
+	fpsGraph:update(dt)
+	memGraph:update(dt)
+end
+
+function love.draw()
+	fpsGraph:draw()
+	memGraph:draw()
+end
