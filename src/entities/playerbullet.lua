397ae5b00a8c1bc03eb73bdc4a9e654a91c73d3e
charlie <charlieschuy@gmail.com>
2018-04-09 14:21:26 -0700

    Add a minimum speed to player bullets

--- src/entities/playerbullet.lua
+++ src/entities/playerbullet.lua
@@ -4,6 +4,8 @@ local Timer = require "lib.hump.timer"
 local Bullet = require "src.entities.bullet"
 local Collision = require "src.util.collision"
 
+local math = math
+
 local PlayerBullet = class("PlayerBullet", Bullet)
 function PlayerBullet:initialize(x, y, direction, playerVelocity, world, collisionWorld)
     Bullet.initialize(self, x, y, 4, 4, world, collisionWorld)
@@ -12,14 +14,14 @@ function PlayerBullet:initialize(x, y, direction, playerVelocity, world, collisi
     self.aabb.filter = Collision.defaultFilter
 
     if direction == "right" then
-        self.velocity.x = 4 + playerVelocity.x
+        self.velocity.x = math.max(4, 4 + playerVelocity.x)
         self.velocity.y = 0
     elseif direction == "left" then
-        self.velocity.x = -4 + playerVelocity.x
+        self.velocity.x = math.min(-4, -4 + playerVelocity.x)
         self.velocity.y = 0
     elseif direction == "up" then
         self.velocity.x = 0
-        self.velocity.y = -4 + playerVelocity.y
+        self.velocity.y = math.min(-4, -4 + playerVelocity.y)
     end
 
     Timer.after(0.4, function()
